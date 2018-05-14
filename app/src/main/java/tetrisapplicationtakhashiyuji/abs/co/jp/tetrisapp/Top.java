10fa19326eb4e2fc650d94a663fd11c7ea7f8217
takahashiyuji <darking7890@gmail.com>
2018-05-14 10:43:01 +0900

    Initial commit

---
+++ app/src/main/java/tetrisapplicationtakhashiyuji/abs/co/jp/tetrisapp/Top.java
@@ -0,0 +1,13 @@
+package tetrisapplicationtakhashiyuji.abs.co.jp.tetrisapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Top extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top);
+    }
+}
